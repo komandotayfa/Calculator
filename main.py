46568046b0a6b9fe4f2a77aac70d97b5d7bc8a8b
komandotayfa <53448093+komandotayfa@users.noreply.github.com>
2023-12-15 20:33:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import toplama,carpma,bolme,cikarma
-num1 = int(input("1. sayı: "))
-num2 = int(input("2. sayı: "))
-islem = str(input("İşlemi giriniz: "))
-match islem:
-    case "*":
-        print(carpma.carp(num1,num2))
-    case "/":
-        print(bolme.bolme(num1,num2))
-    case "-":
-        print(cikarma.cikar(num1,num2))
-    case "+":
-        print(toplama.toplam(num1,num2))
+while True:
+    num1 = int(input("1. sayı: "))
+    num2 = int(input("2. sayı: "))
+    islem = str(input("İşlemi giriniz: "))
+    match islem:
+        case "*":
+            print(carpma.carp(num1,num2))
+        case "/":
+            print(bolme.bolme(num1,num2))
+        case "-":
+            print(cikarma.cikar(num1,num2))
+        case "+":
+            print(toplama.toplam(num1,num2))
